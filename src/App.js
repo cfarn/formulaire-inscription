b39adf6b4ca222aadaa85101c5cb652830d382f6
cfarn <cinniefarnaud@gmail.com>
2022-10-03 16:01:00 +0200

    validation

--- src/App.js
+++ src/App.js
@@ -20,8 +20,26 @@ const App = () => {
     },
     validationSchema: Yup.object().shape({
       email: Yup.string()
-       .required("Email requis")
-    })
+        .required("Email requis")
+        .email("Email invalide"),
+      firstName: Yup.string()
+        .required("Prénom requis"),
+      lastName: Yup.string()
+        .required("Nom requis"),
+      userName: Yup.string()
+        .required("Pseudo requis")
+        .min(4, "Pseudo trop court"),
+      password: Yup.string()
+        .required("Password requis")
+        .min(8, "Password trop court"),
+      passwordConfirmation: Yup.string()
+        .required("Champ requis"),
+        // .passwordConfirmation === password,
+      birthdate: Yup.date()
+        .required("Champ requis")
+        
+    }),
+    validateOnChange: false
   })
 
   return (
@@ -31,55 +49,63 @@ const App = () => {
         type="email"
         placeholder="Enter your email"
         value={formik.values.email}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.email}
       />
       <Input
         name="firstName"
         type="text"
         placeholder="Enter your firstname"
         value={formik.values.firstName}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.firstName}
       />
       <Input
         name="lastName"
         type="text"
         placeholder="Enter your lastname"
         value={formik.values.lastName}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.lastName}
       />
       <Input
         name="userName"
         type="text"
         placeholder="Enter your username"
         value={formik.values.userName}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.userName}
       />
       <Input
         name="password"
         type="password"
         placeholder="Enter your password"
         value={formik.values.password}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.password}
       />
       <Input
         name="passwordConfirmation"
         type="password"
         placeholder="Confirm your password"
         value={formik.values.passwordConfirmation}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.passwordConfirmation}
       />
       <Input
         name="birthdate"
         type="date"
         value={formik.values.birthdate}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.birthdate}
       />
       <Input
         name="github"
         type="url"
         placeholder="Enter your github url"
         value={formik.values.github}
-        onChange={formik.handleChange}
+        handleChange={formik.handleChange}
+        error={formik.errors.github}
       />
       <button type="submit">Submit</button>
     </form>
